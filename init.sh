5ad3e6b0a0728b8bc05bab99c136fe05c586f331
John Ott <john.ott@johnott.us>
2017-01-25 10:23:32 -0800

    Add MQTT_IP

--- init.sh
+++ init.sh
@@ -6,7 +6,9 @@ fi
 export REPO_DIR=$(cd "$(dirname "$0")" && pwd)
 export BIN_DIR="${REPO_DIR}/tools/bin"
 export PATH="${BIN_DIR}:${PATH}"
+export MQTT_IP="192.168.0.100"
 
 echo "REPO_DIR=${REPO_DIR}"
 echo "BIN_DIR=${BIN_DIR}"
+echo "MQTT_IP=${MQTT_IP}"
 echo 'Added $BIN_DIR to path!'
